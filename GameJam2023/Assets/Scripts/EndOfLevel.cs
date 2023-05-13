fdab4b3083108765e5848859d64812092888c883
RENNES\o.leclerc <orion.leclerc@gmail.com>
2023-05-13 20:36:45 +0200

    Scene change

---
+++ GameJam2023/Assets/Scripts/EndOfLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour
+{
+    [SerializeField] private SceneChanger _sceneChanger;
+    [SerializeField] private string _sceneToLoad;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var playerController = collision.GetComponent<PlayerController>();
+        if(playerController != null)
+        {
+            _sceneChanger.LoadAScene(_sceneToLoad);
+        }
+    }
+}
